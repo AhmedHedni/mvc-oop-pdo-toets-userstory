13568ed3d0a899edbecba10b791d053aadbb2ba2
Ahmed <90773338+Ahmedscorro@users.noreply.github.com>
2023-01-12 12:38:11 +0100

    Landigpages pagina gefixt.

--- app/controllers/Landingpages.php
+++ app/controllers/Landingpages.php
@@ -6,7 +6,7 @@ class Landingpages extends Controller
     {
         $data = [
             'title' => 'Homepage MVC',
-            'sayHello' => 'Hallo Allemaal'
+            'sayHello' => 'Hallo'
         ];
         $this->view('landingpages/index', $data);
     }
